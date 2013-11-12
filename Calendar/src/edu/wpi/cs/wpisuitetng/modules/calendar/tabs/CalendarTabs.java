4b82a4f92589b98abb83e0923590301e813b3a0b
Devon Locke <djlocke@wpi.edu>
2013-11-11 22:20:49 -0500

    I got the tabs working correctly

--- Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/tabs/CalendarTabs.java
+++ Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/tabs/CalendarTabs.java
@@ -6,12 +6,23 @@ import edu.wpi.cs.wpisuitetng.modules.calendar.view.*;
 
 public class CalendarTabs extends JTabbedPane {
 	private final MonthView monthPanel;
-
+	private final MonthView monthPanel2; // these next three should become the other views
+	private final MonthView monthPanel3;
+	private final MonthView monthPanel4;
+	
+	
 	public CalendarTabs() {
 		// TODO Auto-generated constructor stub
 		monthPanel = new MonthView();
+		monthPanel2 = new MonthView(); // these next three should be the other views when implemented
+		monthPanel3 = new MonthView();
+		monthPanel4 = new MonthView();
 		
+		this.addTab("Year", monthPanel2);
 		this.addTab("Month", monthPanel);
+		this.addTab("Week", monthPanel3);
+		this.addTab("Day", monthPanel4);
+		
 		
 	}
 /*
