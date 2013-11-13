10f1087eea99797af244ce9b1c6accc6280b3237
Johnny Hernandez <jxhernandez@wpi.edu>
2013-11-12 21:30:18 -0500

    Fixed some commenting on MonthView for Audit

--- Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/view/MonthView.java
+++ Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/view/MonthView.java
@@ -1,3 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2012 -- WPI Suite
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors: Team Underscore 
+ *  DreamInCode user: Alpha02
+ *    
+ *******************************************************************************/
 package edu.wpi.cs.wpisuitetng.modules.calendar.view;
 
 import javax.swing.*;
@@ -23,32 +35,19 @@ public class MonthView extends JPanel {
 		setLayout(null);
 		// Set look and feel will make the look of the Janeway different on all fronts
 		// Leave the look and feel default for now, make it prettier later
-		// setLookAndFeel();
 		createControls();
-		setBorder();
+		createAndSetBorder();
 		registerActionListeners();
 		addControls();
-		setBounds();
-		setDate();
+		createBounds();
+		createDate();
 		addHeaders();
-		setBackground();
-		setTableProperties();
+		createBackground();
+		createTableProperties();
 		populateTable();
 		refreshCalendar(realMonth, realYear);
     }
-
-    /*
-    // Sets the look and feel based on operating system, if supported by Java, if not just get the defaults
-    private void setLookAndFeel() {
-	try {
-	    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-	} catch (ClassNotFoundException e) {
-	} catch (InstantiationException e) {
-	} catch (IllegalAccessException e) {
-	} catch (UnsupportedLookAndFeelException e) {
-	}
-    }
-	*/
+    
     // Create the UI controls for the Calendar and surrounding components
     // Buttons to change month
     // Dropdown to change year 
@@ -68,13 +67,12 @@ public class MonthView extends JPanel {
 	    }
 	};
 	tblCalendar = new JTable(mtblCalendar);
-	//I can not for the life of me make the cells selectable, this doesn't work may have something to do with the renderer
-	//tblCalendar.setCellSelectionEnabled(true);
+	//I can not for the life of me make the cells selectable, setSelectable(true) does not work
 	stblCalendar = new JScrollPane(tblCalendar);
     }
 
-    private void setBorder() {
-	this.setBorder(BorderFactory.createTitledBorder("Calendar"));
+    private void createAndSetBorder() {
+    	this.setBorder(BorderFactory.createTitledBorder("Calendar"));
     }
 
     // Set the action listenrer for the button components 
@@ -93,7 +91,7 @@ public class MonthView extends JPanel {
 		this.add(stblCalendar);
     }
 
-    private void setBounds() {
+    private void createBounds() {
 		this.setBounds(0, 0, 626, 600);
 		lblMonth.setBounds(88, 25, 100, 25);
 		lblYear.setBounds(520, 230, 80, 20);
@@ -103,8 +101,8 @@ public class MonthView extends JPanel {
 		stblCalendar.setBounds(10, 50, 500, 420);
     }
 
-    private void setDate() {
-		GregorianCalendar cal = new GregorianCalendar(); // Create calendar
+    private void createDate() {
+		final GregorianCalendar cal = new GregorianCalendar(); // Create calendar
 		realDay = cal.get(GregorianCalendar.DAY_OF_MONTH); // Get day
 		realMonth = cal.get(GregorianCalendar.MONTH); // Get month
 		realYear = cal.get(GregorianCalendar.YEAR); // Get year
@@ -113,17 +111,17 @@ public class MonthView extends JPanel {
     }
 
     private void addHeaders() {
-	String[] headers = { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
+	final String[] headers = { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
 	for (int i = 0; i < 7; i++) {
 	    mtblCalendar.addColumn(headers[i]);
-	}
+		}
     }
 
-    private void setBackground() {
-	tblCalendar.getParent().setBackground(tblCalendar.getBackground());
+    private void createBackground() {
+    	tblCalendar.getParent().setBackground(tblCalendar.getBackground());
     }
 
-    private void setTableProperties() {
+    private void createTableProperties() {
 	// No resize/reorder
 	tblCalendar.getTableHeader().setResizingAllowed(false);
 	tblCalendar.getTableHeader().setReorderingAllowed(false);
@@ -147,10 +145,11 @@ public class MonthView extends JPanel {
 
     private static void refreshCalendar(int month, int year) {
 	// Variables
-	String[] months = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
-	int nod, som; // Number Of Days, Start Of Month
+	final String[] months = { "January", "February", "March", "April", "May", "June", "July",
+			"August", "September", "October", "November", "December" };
+	final int nod, som; // Number Of Days, Start Of Month
 
-	// Allow/disallow buttons
+	// Allow (or disallow) buttons
 	btnPrev.setEnabled(true);
 	btnNext.setEnabled(true);
 	if (month == 0 && year <= realYear - 10) {
@@ -167,7 +166,8 @@ public class MonthView extends JPanel {
 	cmbYear.setSelectedItem(String.valueOf(year)); 
 
 	// Clear table
-	// THIS NEEDS MODIFICATION TO SUPPORT BUTTON CELLS IN THE CALENDAR MAKE CHANGES HERE TO REMOVE BUTTONS 
+	// THIS NEEDS MODIFICATION TO SUPPORT BUTTON CELLS IN THE CALENDAR 
+	// MAKE CHANGES HERE TO REMOVE BUTTONS 
 	for (int i = 0; i < 6; i++) {
 	    for (int j = 0; j < 7; j++) {
 		mtblCalendar.setValueAt(null, i, j);
@@ -175,7 +175,7 @@ public class MonthView extends JPanel {
 	}
 
 	// Get first day of month and number of days
-	GregorianCalendar cal = new GregorianCalendar(year, month, 1);
+	final GregorianCalendar cal = new GregorianCalendar(year, month, 1);
 	nod = cal.getActualMaximum(GregorianCalendar.DAY_OF_MONTH);
 	som = cal.get(GregorianCalendar.DAY_OF_WEEK);
 
@@ -194,26 +194,22 @@ public class MonthView extends JPanel {
     static class tblCalendarRenderer extends DefaultTableCellRenderer {
 	private static final long serialVersionUID = 1L;
 
-	public Component getTableCellRendererComponent(JTable table, Object value, boolean selected, boolean focused, int row, int column) {
+	public Component getTableCellRendererComponent(JTable table, Object value, boolean selected,
+			boolean focused, int row, int column) {
 	    super.getTableCellRendererComponent(table, value, selected, focused, row, column);
 	    
 	    // Adding this code allows for selection to be used on this component
 	    // For now I think this should work in place of event listeners
-	    // Apparently doesn't work 
-	    // SHOULD LOOK AT THIS LATER
-	    /*
-	    if (selected == true) {
-	    	setBackground(new Color(220,220,0));
-	    }
-	    */
-	    if (column == 0 || column == 6) { // Week-end
+	    // an if statement checking if selected should work in theory
+	    if (column == 0 || column == 6) { // Highlight the week-end
 		setBackground(new Color(255, 220, 220));
 	    }
 	    else { // Week
-		setBackground(new Color(255, 255, 255));
+		setBackground(Color.white);
 	    }
 	    if (value != null) {
-		if (Integer.parseInt(value.toString()) == realDay && currentMonth == realMonth && currentYear == realYear) { // Today
+		if (Integer.parseInt(value.toString()) == realDay && currentMonth == realMonth 
+				&& currentYear == realYear) { // Today
 		    setBackground(new Color(220, 220, 255));
 		}
 	    }
@@ -252,7 +248,7 @@ public class MonthView extends JPanel {
     static class cmbYear_Action implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 	    if (cmbYear.getSelectedItem() != null) {
-		String b = cmbYear.getSelectedItem().toString();
+		final String b = cmbYear.getSelectedItem().toString();
 		currentYear = Integer.parseInt(b);
 		refreshCalendar(currentMonth, currentYear);
 	    }
