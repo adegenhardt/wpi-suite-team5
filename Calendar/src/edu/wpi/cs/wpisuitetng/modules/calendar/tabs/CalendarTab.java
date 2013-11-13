6cb5d1b110bab10dba30f3af023388a29800a867
Devon Locke <djlocke@wpi.edu>
2013-11-12 19:09:30 -0500

    created year tab

--- Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/tabs/CalendarTab.java
+++ Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/tabs/CalendarTab.java
@@ -1,3 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2012 -- WPI Suite
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors: Team Underscore 
+ *    
+ *******************************************************************************/
+
 package edu.wpi.cs.wpisuitetng.modules.calendar.tabs;
 
 
@@ -7,26 +19,18 @@ import edu.wpi.cs.wpisuitetng.modules.calendar.view.*;
 
 public class CalendarTab extends JTabbedPane {
 	private final MonthView monthPanel;
+	private final YearViewCalendarPanel yearPanel;
 	
 	
 	
 	public CalendarTab() {
-		// TODO Auto-generated constructor stub
 		monthPanel = new MonthView();
+		yearPanel= new YearViewCalendarPanel();
 		
+		this.addTab("Year", yearPanel);
 		this.addTab("Month", monthPanel);
 		
 		
+		
 	}
-/*
-	public CalendarTabs(int tabPlacement) {
-		super(tabPlacement);
-		// TODO Auto-generated constructor stub
-	}
-
-	public CalendarTabs(int tabPlacement, int tabLayoutPolicy) {
-		super(tabPlacement, tabLayoutPolicy);
-		// TODO Auto-generated constructor stub
-	}
-*/
 }
