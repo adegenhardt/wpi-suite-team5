e17367033dafe3686a20a561fd598bda2f5f94b5
hill1193 <jchill@wpi.edu>
2013-11-18 20:09:48 -0500

    Fixed some comments

--- Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/models/category/Category.java
+++ Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/models/category/Category.java
@@ -1,7 +1,19 @@
+/*******************************************************************************
+ * Copyright (c) 2013 -- WPI Suite
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors: Team Underscore 
+ *    
+ *******************************************************************************/
+
 package edu.wpi.cs.wpisuitetng.modules.calendar.models.category;
 
 /** Category class that contains all the fields required to create a category
- * @author Team_
+ * @author Joe Hill
  *
  */
 
