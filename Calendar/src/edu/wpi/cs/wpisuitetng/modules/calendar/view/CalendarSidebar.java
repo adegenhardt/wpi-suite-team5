217768f62c444d324a18186eea92d30f69d496f0
Tyler M <nfreakdq@gmail.com>
2013-11-22 13:37:34 -0500

    Updated sidebar

--- Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/view/CalendarSidebar.java
+++ Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/view/CalendarSidebar.java
@@ -26,6 +26,9 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JRadioButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JComboBox;
 
 /**
  * @author Team Underscore
@@ -40,15 +43,16 @@ public class CalendarSidebar extends JPanel {
 	private JTable eventTable;
 	private JTable commitmentTable;
 	private JTextField textField;
+	private JTextField filterTextField;
 
 	/**
 	 * Create the panel.
 	 */
 	public CalendarSidebar() {
-		setLayout(new MigLayout("", "[grow]", "[10:237.00:3853294,center][246.00,grow][grow][grow][grow][grow][]"));
+		setLayout(new MigLayout("", "[grow][grow]", "[100.00,grow,center][100.00,grow][grow]"));
 
 		final JScrollPane eventScroll = new JScrollPane();
-		add(eventScroll, "cell 0 0,grow");
+		add(eventScroll, "cell 0 0 2 1,grow");
 		
 		eventTable = new JTable();
 		eventScroll.setViewportView(eventTable);
@@ -61,30 +65,14 @@ public class CalendarSidebar extends JPanel {
 				{null, null, null, null},
 				{null, null, null, null},
 				{null, null, null, null},
-				{null, null, null, null},
-				{null, null, null, null},
-				{null, null, null, null},
-				{null, null, null, null},
-				{null, null, null, null},
 			},
 			new String[] {
 				"Events", "Date", "Start Time", "End Time"
 			}
-		) {
-			/**
-			 * 
-			 */
-			private static final long serialVersionUID = 1L;
-			private final boolean[] columnEditables = new boolean[] {
-				false, false, false, false
-			};
-			public boolean isCellEditable(int row, int column) {
-				return columnEditables[column];
-			}
-		});
+		));
 		
-		final JScrollPane scrollPane_1 = new JScrollPane();
-		add(scrollPane_1, "cell 0 1,grow");
+		final JScrollPane commitScroll = new JScrollPane();
+		add(commitScroll, "cell 0 1 2 1,grow");
 		
 		commitmentTable = new JTable();
 		commitmentTable.setModel(new DefaultTableModel(
@@ -96,39 +84,27 @@ public class CalendarSidebar extends JPanel {
 				{null, null, null, null},
 				{null, null, null, null},
 				{null, null, null, null},
-				{null, null, null, null},
-				{null, null, null, null},
-				{null, null, null, null},
-				{null, null, null, null},
-				{null, null, null, null},
 			},
 			new String[] {
 				"Commitment", "Date", "Category ", "Description"
 			}
-		) {
-			/**
-			 * 
-			 */
-			private static final long serialVersionUID = 1L;
-			private final boolean[] columnEditables = new boolean[] {
-				false, false, false, false
-			};
-			public boolean isCellEditable(int row, int column) {
-				return columnEditables[column];
-			}
-		});
-		scrollPane_1.setViewportView(commitmentTable);
+		));
+		commitScroll.setViewportView(commitmentTable);
 		
 		JScrollPane scrollPane = new JScrollPane();
-		add(scrollPane, "cell 0 2,grow");
+		add(scrollPane, "cell 0 2 2 1,grow");
+		
+		JPanel filtersCatsPanel = new JPanel();
+		scrollPane.setViewportView(filtersCatsPanel);
+		filtersCatsPanel.setLayout(new MigLayout("", "[grow]", "[][]"));
 		
 		JPanel panelFilter = new JPanel();
-		scrollPane.setViewportView(panelFilter);
+		filtersCatsPanel.add(panelFilter, "cell 0 0,grow");
 		panelFilter.setBorder(new TitledBorder(null, "Filters", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		panelFilter.setLayout(new MigLayout("", "[grow]", "[grow][grow][]"));
+		panelFilter.setLayout(new MigLayout("", "[grow][grow]", "[85.00px][][]"));
 		
 		JList list = new JList();
-		panelFilter.add(list, "cell 0 1,grow");
+		panelFilter.add(list, "cell 0 0,grow");
 		list.setModel(new AbstractListModel() {
 			String[] values = new String[] {"Team", "Personal", "Things"};
 			public int getSize() {
@@ -140,37 +116,51 @@ public class CalendarSidebar extends JPanel {
 		});
 		
 		JButton btnApply = new JButton("Apply");
-		panelFilter.add(btnApply, "flowx,cell 0 2");
+		panelFilter.add(btnApply, "flowx,cell 0 1,alignx left");
 		
-		JButton btnDelete = new JButton("Delete");
-		panelFilter.add(btnDelete, "cell 0 2");
+		filterTextField = new JTextField();
+		panelFilter.add(filterTextField, "cell 0 2,growx");
+		filterTextField.setColumns(10);
 		
 		JButton btnNewFilter = new JButton("New Filter");
-		panelFilter.add(btnNewFilter, "cell 0 2");
+		panelFilter.add(btnNewFilter, "cell 1 2,alignx left");
 		
-		JScrollPane scrollPane_2 = new JScrollPane();
-		add(scrollPane_2, "cell 0 3,grow");
+		JButton btnDelete = new JButton("Delete");
+		panelFilter.add(btnDelete, "cell 0 1,alignx left");
 		
 		JPanel panelCatCreate = new JPanel();
-		scrollPane_2.setViewportView(panelCatCreate);
-		panelCatCreate.setBorder(new TitledBorder(null, "Create a Category", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		panelCatCreate.setLayout(new MigLayout("", "[][][]", "[][][][]"));
+		filtersCatsPanel.add(panelCatCreate, "cell 0 1,grow");
+		panelCatCreate.setBorder(new TitledBorder(null, "Categories", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panelCatCreate.setLayout(new MigLayout("", "[80.00,grow][100px,grow][]", "[][][][]"));
 		
-		JLabel lblCategory = new JLabel("Category:");
-		panelCatCreate.add(lblCategory, "cell 0 0,alignx trailing");
+		JLabel lblCurrentCategories = new JLabel("Categories:");
+		panelCatCreate.add(lblCurrentCategories, "cell 0 0,alignx right");
+		
+		JComboBox comboBox = new JComboBox();
+		panelCatCreate.add(comboBox, "cell 1 0,growx");
+		
+		JButton btnDelete_1 = new JButton("Delete");
+		panelCatCreate.add(btnDelete_1, "cell 1 1,alignx left");
+		
+		JLabel lblCategory = new JLabel("New Category:");
+		panelCatCreate.add(lblCategory, "cell 0 2,alignx trailing");
 		
 		textField = new JTextField();
-		panelCatCreate.add(textField, "cell 1 0 2 1,growx");
+		panelCatCreate.add(textField, "cell 1 2,growx");
 		textField.setColumns(10);
 		
 		JRadioButton rdbtnTeam = new JRadioButton("Team");
-		panelCatCreate.add(rdbtnTeam, "flowx,cell 1 1");
+		panelCatCreate.add(rdbtnTeam, "flowx,cell 1 3");
 		
 		JRadioButton rdbtnPersonal = new JRadioButton("Personal");
-		panelCatCreate.add(rdbtnPersonal, "cell 1 1");
+		panelCatCreate.add(rdbtnPersonal, "cell 1 3");
 		
 		JButton btnSubmit = new JButton("Submit");
-		panelCatCreate.add(btnSubmit, "cell 2 1");
+		btnSubmit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		panelCatCreate.add(btnSubmit, "cell 1 3,growx");
 
 	}
 
