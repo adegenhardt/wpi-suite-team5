e775a7121b9fff5ae3d6d02da2b02503846d508d
Alexander W. Witt <awitt@wpi.edu>
2013-12-13 20:50:00 -0500

    Addition of RefreshListenerUponEventCreation class.

---
+++ Calendar/src/Refresh/RefreshListenerUponEventCreation.java
@@ -0,0 +1,27 @@
+package Refresh;
+import javax.swing.event.ListDataEvent;
+import javax.swing.event.ListDataListener;
+
+import edu.wpi.cs.wpisuitetng.modules.calendar.view.CalendarSidebar;
+
+
+ public class RefreshListenerUponEventCreation implements ListDataListener {
+	@Override
+	public void intervalAdded(ListDataEvent e) {
+		CalendarSidebar.getInstance().populateTable();
+		
+	}
+
+	@Override
+	public void intervalRemoved(ListDataEvent e) {
+		CalendarSidebar.getInstance().populateTable();
+		
+	}
+
+	@Override
+	public void contentsChanged(ListDataEvent e) {
+		CalendarSidebar.getInstance().populateTable();
+		
+	}
+	
+}
