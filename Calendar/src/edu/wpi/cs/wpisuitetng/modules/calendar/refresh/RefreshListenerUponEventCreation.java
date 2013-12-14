6b374b76d36f4a676de439528f38398dd663571d
Johnny Hernandez <jxhernandez@wpi.edu>
2013-12-13 23:55:26 -0500

    OKAY ALL OF THE VIEWS NOW UPDATE ON ADDITION OF EVENTS HAHAHAH

--- Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/refresh/RefreshListenerUponEventCreation.java
+++ Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/refresh/RefreshListenerUponEventCreation.java
@@ -3,25 +3,34 @@ import javax.swing.event.ListDataEvent;
 import javax.swing.event.ListDataListener;
 
 import edu.wpi.cs.wpisuitetng.modules.calendar.view.CalendarSidebar;
+import edu.wpi.cs.wpisuitetng.modules.calendar.view.calendars.DayView;
+import edu.wpi.cs.wpisuitetng.modules.calendar.view.calendars.WeekView;
+import edu.wpi.cs.wpisuitetng.modules.calendar.view.calendars.YearViewCalendar;
 
 
  public class RefreshListenerUponEventCreation implements ListDataListener {
 	@Override
 	public void intervalAdded(ListDataEvent e) {
 		CalendarSidebar.getInstance().populateTable();
-		
+		DayView.getInstance().refreshEvents();
+		YearViewCalendar.getInstance(null).refreshYear();
+		WeekView.getInstance().refreshEvents();
 	}
 
 	@Override
 	public void intervalRemoved(ListDataEvent e) {
 		CalendarSidebar.getInstance().populateTable();
-		
+		DayView.getInstance().refreshEvents();
+		YearViewCalendar.getInstance(null).refreshYear();
+		WeekView.getInstance().refreshEvents();
 	}
 
 	@Override
 	public void contentsChanged(ListDataEvent e) {
 		CalendarSidebar.getInstance().populateTable();
-		
+		DayView.getInstance().refreshEvents();
+		YearViewCalendar.getInstance(null).refreshYear();
+		WeekView.getInstance().refreshEvents();
 	}
 	
 }
